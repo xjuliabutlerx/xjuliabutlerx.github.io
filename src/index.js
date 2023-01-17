b6423c5d18c44dd5176f9551f16296dc0fb5f56b
juliabutler <juliafulibutler@gmail.com>
2023-01-17 13:34:10 -0500

    Adding new font

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { HashRouter as Router } from "react-router-dom";
+import './fonts/Sacramento/Sacramento-Regular.ttf';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
